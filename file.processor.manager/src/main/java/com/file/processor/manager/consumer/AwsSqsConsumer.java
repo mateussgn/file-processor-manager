fcabfa90897b97548440281fb9ef11776530952d
Mateus Santana <mateus-sgn@hotmail.com>
2024-09-15 02:11:46 -0300

    chore: adjust consumer to avoid empty messages

--- file.processor.manager/src/main/java/com/file/processor/manager/consumer/AwsSqsConsumer.java
+++ file.processor.manager/src/main/java/com/file/processor/manager/consumer/AwsSqsConsumer.java
@@ -1,14 +1,30 @@
 package com.file.processor.manager.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.file.processor.manager.dto.QueueFileMetadata;
+import com.file.processor.manager.service.AwsDynamoDbService;
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AwsSqsConsumer {
 
+    AwsDynamoDbService awsDynamoDbService;
+
+    @Autowired
+    public AwsSqsConsumer(AwsDynamoDbService awsDynamoDbService) {
+        this.awsDynamoDbService = awsDynamoDbService;
+    }
+
     @SqsListener("process-file-queue")
-    public void consume(QueueFileMetadata queueFileMetadata) {
+    public void consume(QueueFileMetadata queueFileMetadata) throws JsonProcessingException {
         System.out.println("Message Received: \n" + queueFileMetadata.content());
+
+        if (queueFileMetadata.content().equals("[]")) {
+            return;
+        }
+
+        awsDynamoDbService.save(queueFileMetadata);
     }
 }
