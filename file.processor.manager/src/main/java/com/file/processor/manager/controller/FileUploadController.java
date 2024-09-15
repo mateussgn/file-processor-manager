ea2ed05fd4f2af2be076636916f5defbf49687b1
Mateus Santana <mateus-sgn@hotmail.com>
2024-09-14 23:32:02 -0300

    chore: publish file metadata on sqs

--- file.processor.manager/src/main/java/com/file/processor/manager/controller/FileUploadController.java
+++ file.processor.manager/src/main/java/com/file/processor/manager/controller/FileUploadController.java
@@ -1,6 +1,8 @@
 package com.file.processor.manager.controller;
 
 import com.file.processor.manager.dto.FileMetadata;
+import com.file.processor.manager.producer.AwsSqsProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/files")
 public class FileUploadController {
 
+    @Autowired
+    AwsSqsProducer awsSqsProducer;
+
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestBody FileMetadata fileMetadata) {
+    public ResponseEntity<String> uploadFile(@RequestBody FileMetadata fileMetadata) throws Exception {
 
         System.out.println(fileMetadata);
+
+        awsSqsProducer.run(fileMetadata.toString());
 
         return ResponseEntity.ok("File uploaded successfully.");
     }
