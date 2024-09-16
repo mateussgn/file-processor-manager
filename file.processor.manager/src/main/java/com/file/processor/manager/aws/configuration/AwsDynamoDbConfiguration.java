625232c307aca659a0cef31d70b75123560112c0
Mateus Santana <mateus-sgn@hotmail.com>
2024-09-16 03:04:43 -0300

    chore: adjust dynamo db configuration

--- file.processor.manager/src/main/java/com/file/processor/manager/aws/configuration/AwsDynamoDbConfiguration.java
+++ file.processor.manager/src/main/java/com/file/processor/manager/aws/configuration/AwsDynamoDbConfiguration.java
@@ -10,12 +10,9 @@ import java.net.URI;
 @Configuration
 public class AwsDynamoDbConfiguration {
 
-    private static final String DYNAMO_DB_URI = "http://localhost:4566";
-
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .endpointOverride(URI.create(DYNAMO_DB_URI))
                 .region(Region.US_EAST_1)
                 .build();
     }
