ac0e945433dfeaa2fee4edf5f3c0526fe94c81fc
Mateus Santana <mateus-sgn@hotmail.com>
2024-09-16 04:08:25 -0300

    chore: remove unused methods

--- file.processor.manager/src/main/java/com/file/processor/manager/service/AwsDynamoDbService.java
+++ file.processor.manager/src/main/java/com/file/processor/manager/service/AwsDynamoDbService.java
@@ -9,8 +9,6 @@ import io.awspring.cloud.dynamodb.DynamoDbTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 public class AwsDynamoDbService {
 
@@ -21,25 +19,7 @@ public class AwsDynamoDbService {
         this.dynamoDbTemplate = dynamoDbTemplate;
     }
 
-    public void save(QueueFileMetadata queueFileMetadata) throws JsonProcessingException {
-        FileMetadataModel fileMetadataModel = toFileMetadataModel(queueFileMetadata.content());
+    public void save(FileMetadataModel fileMetadataModel) throws JsonProcessingException {
         dynamoDbTemplate.save(fileMetadataModel);
-    }
-
-    public FileMetadataModel toFileMetadataModel(String content) throws JsonProcessingException {
-        FileMetadata fileMetadata = toFileMetadata(content.substring(1, content.length() - 1));
-
-        return new FileMetadataModel(
-                fileMetadata.filename(),
-                fileMetadata.fileSize(),
-                fileMetadata.fileType(),
-                fileMetadata.userId(),
-                fileMetadata.email()
-        );
-    }
-
-    private FileMetadata toFileMetadata(String jsonString) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(jsonString, FileMetadata.class);
     }
 }
